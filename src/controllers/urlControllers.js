e141f1af23b91b73fa2cdfbdf1c3c888543387d9
joaofelix27 <joaofelix27@gmail.com>
2022-08-05 16:47:44 -0300

    Rota Delete Ok

--- src/controllers/urlControllers.js
+++ src/controllers/urlControllers.js
@@ -1,23 +1,78 @@
-import connection from "../db/postgres.js"
-import { nanoid } from 'nanoid'
+import connection from "../db/postgres.js";
+import { nanoid } from "nanoid";
 
-export async function urlsPost (req,res) {
-    const {url}=req.body
-    const shortUrl = nanoid();
-    try{
-        await connection.query(`INSERT INTO urls (url,"shortUrl") VALUES ($1,$2)`,[url,shortUrl])
-        res.status(201).json({shortUrl: shortUrl})
-    } catch{
-        res.sendStatus(500)
-    }
+export async function urlsPost(req, res) {
+  const { url } = req.body;
+  const shortUrl = nanoid();
+  const userId=req.userId
+  try {
+    await connection.query(`INSERT INTO urls (url,"shortUrl","userId") VALUES ($1,$2,$3)`, [
+      url,
+      shortUrl,userId
+    ]);
+    res.status(201).json({ shortUrl: shortUrl });
+  } catch(e) {
+    res.status(500).send(e.message);
+  }
 }
-export async function getUrlsById (req,res) {
-    const {url}=req.body
-    const shortUrl = nanoid();
-    try{
-        await connection.query(`INSERT INTO urls (url,"shortUrl") VALUES ($1,$2)`,[url,shortUrl])
-        res.status(201).json({shortUrl: shortUrl})
-    } catch{
-        res.sendStatus(500)
+export async function getUrlsById(req, res) {
+  const { id } = req.params;
+  try {
+    const { rows: findUrl } = await connection.query(
+      `SELECT * from urls WHERE Id='${id}'`
+    );
+    const findUrlLength = findUrl.length;
+    if (findUrlLength === 1) {
+      return res.status(200).send(findUrl[0]);
+    } else {
+      return res.sendStatus(404);
     }
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+}
+export async function getUrlByShortenUrl(req, res) {
+  const { shortUrl } = req.params;
+  try {
+    const { rows: findUrl } = await connection.query(
+      `SELECT * from urls WHERE "shortUrl"='${shortUrl}'`
+    );
+    const findUrlLength = findUrl.length;
+    if (findUrlLength === 1) {
+      const visitCount = findUrl[0].visitCount;
+      await connection.query(
+        `UPDATE urls SET "visitCount"=${
+          visitCount + 1
+        }  WHERE "shortUrl"='${shortUrl}'`
+      );
+      return res.redirect(findUrl[0].url);
+    } else {
+      return res.sendStatus(404);
+    }
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+}
+export async function deleteUrlById(req, res) {
+  const { id } = req.params;
+  const userId = req.userId;
+  try {
+    const { rows: findUrl } = await connection.query(
+      `SELECT * from urls WHERE Id='${id}'`
+    );
+    const findUrlLength = findUrl.length;
+    if (findUrlLength === 1) {
+      const userIdDb = findUrl[0]?.userId;
+      if (userId === userIdDb) {
+        await connection.query(`DELETE FROM urls WHERE id=${id};`);
+        return res.sendStatus(204);
+      } else {
+        return res.sendStatus(401);
+      }
+    } else {
+      return res.sendStatus(404);
+    }
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 }
--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { urlsPost } from "../controllers/urlControllers.js";
+import { deleteUrlById, getUrlByShortenUrl, getUrlsById, urlsPost } from "../controllers/urlControllers.js";
 import validateUrl from "../middlewares/urlSchemaMiddleware.js";
 import validateUser from "../middlewares/validateUserMiddleware.js";
 
 const urlRouter=Router()
 
 urlRouter.post("/urls/shorten",validateUrl,validateUser,urlsPost)
+urlRouter.get("/urls/:id",getUrlsById)
+urlRouter.get("/urls/open/:shortUrl",getUrlByShortenUrl)
+urlRouter.delete("/urls/:id",validateUser,deleteUrlById)
 
 export default urlRouter
