7d9444faff8c8fdd6d542c3dc8f156813fc5238d
joaofelix27 <joaofelix27@gmail.com>
2022-08-06 15:36:55 -0300

    Generic Schema Middleware - Bônus 02

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { signUp,signIn } from "../controllers/authControllers.js";
-import validateSignIn from "../middlewares/signInSchemaMiddleware.js";
-import validateSignUp from "../middlewares/signUpSchemaMiddleware.js";
+import validateSchema from "../middlewares/genericSchemaMiddleware.js";
 
 
 const authRouter=Router()
 
-authRouter.post("/signUp",validateSignUp,signUp)
+authRouter.post("/signUp",validateSchema,signUp)
 
-authRouter.post("/signIn",validateSignIn,signIn)
+authRouter.post("/signIn",validateSchema,signIn)
 
 export default authRouter
--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { deleteUrlById, getMineShortenUrl, getUrlByShortenUrl, getUrlsById, urlsPost } from "../controllers/urlControllers.js";
-import validateUrl from "../middlewares/urlSchemaMiddleware.js";
+import validateSchema from "../middlewares/genericSchemaMiddleware.js";
 import validateUser from "../middlewares/validateUserMiddleware.js";
 
 const urlRouter=Router()
 
-urlRouter.post("/urls/shorten",validateUrl,validateUser,urlsPost)
+urlRouter.post("/urls/shorten",validateSchema,validateUser,urlsPost)
 urlRouter.get("/urls/:id",getUrlsById)
 urlRouter.get("/urls/open/:shortUrl",getUrlByShortenUrl)
 urlRouter.get("/users/me",validateUser,getMineShortenUrl)
